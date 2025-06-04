f36d9cbfdc2999057be79931bef7b784b397b570
amogus <110690851+vovan-2007@users.noreply.github.com>
2025-06-04 11:36:43 +0400

    Add files via upload

---
+++ lab5t25/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace lab5t25
+{
+    internal class Program
+    {
+        static int min(int a, int b, int c)
+        {
+            return Math.Min(Math.Min(a, b), c);
+        }
+        static int min(int a, int b, int c, int d)
+        {
+            return Math.Min(min(a, b, c), d);
+        }
+        static int min(int a, int b, int c, int d, int e)
+        {
+            return Math.Min(min(a, b, c, d), e);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(min(10, 8, 5));
+            Console.WriteLine(min(14, 12, 10, 8));
+            Console.WriteLine(min(14, 12, 10, 8, 6));
+            Console.WriteLine("Нажмите любую клвашишу для завершения...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab5t26/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace lab5t26
+{
+    internal class Program
+    {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        static int Add(int a, int b, int c, int d)
+        {
+            return a + b + c + d;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(5, 6));
+            Console.WriteLine(Add(5, 6, 7));
+            Console.WriteLine(Add(5, 6, 7, 8));
+            Console.WriteLine("Нажмите любую клвашишу для завершения...");
+            Console.ReadKey();
+        }
+    }
+}
